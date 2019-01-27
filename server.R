e9cb3c3a01101892081e34d347d6f43b3cb33ebe
Markus Voge <mvoge@posteo.de>
2019-01-27 01:44:04 +0100

    Finish Tageseinnahmen, started with Monatseinnahmen: month names

--- server.R
+++ server.R
@@ -18,36 +18,106 @@ onStop(function() {
   poolClose(pool)
 })
 
+geldformat <- function(betrag) {
+  sprintf("%s €", format(betrag, nsmall = 2, big.mark = ".", decimal.mark = ","))
+}
+
 function(input, output, session) {
+  heute <- Sys.Date()
+  
   output$tbl <- renderTable({
     pool %>% tbl("verkauf") %>% arrange(desc(rechnungs_nr)) %>% head(10) %>% collect()
   })
   
-  output$tageseinnahmen1 <- renderUI({
-    df <- pool %>% tbl("abrechnung_tag") %>%
-      filter(zeitpunkt > !!(Sys.Date())) %>%
-      summarise(brutto = sum(mwst_netto) + sum(mwst_betrag)) %>% collect()
-    paste(df$brutto, "€")
-  })
+  tagesfilter <- function(pool, anzahl_tage) {
+    filter(pool, zeitpunkt > !!(heute - anzahl_tage) &
+             zeitpunkt < !!(heute - (anzahl_tage - 1)))
+  }
   
-  output$tageseinnahmen2 <- renderUI({
-    df <- pool %>% tbl("abrechnung_tag") %>%
-      filter(zeitpunkt > !!(Sys.Date() - 1) & zeitpunkt < !!(Sys.Date() - 0)) %>%
-      summarise(brutto = sum(mwst_netto) + sum(mwst_betrag)) %>% collect()
-    paste(df$brutto, "€")
-  })
+  heute_filter <- function(pool) { tagesfilter(pool, 0) }
+  gestern_filter <- function(pool) { tagesfilter(pool, 1) }
+  vorgestern_filter <- function(pool) { tagesfilter(pool, 2) }
+  vorvorgestern_filter <- function(pool) { tagesfilter(pool, 3) }
+  heute_vor_einer_woche_filter <- function(pool) { tagesfilter(pool, 7) }
+  gestern_vor_einer_woche_filter <- function(pool) { tagesfilter(pool, 8) }
+  vorgestern_vor_einer_woche_filter <- function(pool) { tagesfilter(pool, 9) }
+  vorvorgestern_vor_einer_woche_filter <- function(pool) { tagesfilter(pool, 10) }
   
-  output$tageseinnahmen3 <- renderUI({
-    df <- pool %>% tbl("abrechnung_tag") %>%
-      filter(zeitpunkt > !!(Sys.Date() - 2) & zeitpunkt < !!(Sys.Date() - 1)) %>%
-      summarise(brutto = sum(mwst_netto) + sum(mwst_betrag)) %>% collect()
-    paste(df$brutto, "€")
-  })
+  parse_betrag <- function(betrag) {
+    if (is.na(betrag)) betrag <- 0
+    betrag
+  }
   
-  output$tageseinnahmen3 <- renderUI({
+  tageseinnahmen <- function(pool, filter_function) {
     df <- pool %>% tbl("abrechnung_tag") %>%
-      filter(zeitpunkt > !!(Sys.Date() - 3) & zeitpunkt < !!(Sys.Date() - 2)) %>%
+      filter_function() %>%
       summarise(brutto = sum(mwst_netto) + sum(mwst_betrag)) %>% collect()
-    paste(df$brutto, "€")
+    parse_betrag(df$brutto)
+  }
+  
+  tageseinnahmen_live <- function(pool) {
+    # SELECT SUM(ges_preis) FROM kasse.verkauf_details INNER JOIN kasse.verkauf USING (rechnungs_nr) WHERE DATE(verkaufsdatum) = '2019-01-21';
+    df <- pool %>% tbl("verkauf_details") %>%
+      inner_join(tbl(pool, "verkauf"), by = "rechnungs_nr") %>%
+      filter(date(verkaufsdatum) == heute) %>%
+      summarise(brutto = sum(ges_preis)) %>% collect()
+    parse_betrag(df$brutto)
+  }
+  
+  tageseinnahmen_vergleich <- function(dir, vgl) {
+    aenderung <- round((dir - vgl) / vgl * 100)
+    if (vgl == 0) {
+      if (dir == 0) aenderung <- 0 # zero makes sense
+      else return(NULL) # no comparison possible
+    }
+    if (aenderung < 0) {
+      color <- "red"
+      vorzeichen <- "-"
+      icn <- icon("caret-down")
+    } else {
+      color <- "green"
+      if (aenderung == 0) {
+        vorzeichen <- "±"
+        icn <- ""
+      } else {
+        vorzeichen <- "+"
+        icn <- icon("caret-up")
+      }
+    }
+    HTML(sprintf(
+      "<span style='color: %s;'>%s%s%% %s</span></span> <span style='font-size:14px'>ggü. Vorwoche (%s €)</span>",
+      color, vorzeichen, abs(aenderung), icn, vgl
+    ))
+  }
+  
+  output$tageseinnahmen_direkt1 <- renderUI({
+    te <- tageseinnahmen(pool, heute_filter)
+    if (te == 0) {
+      te <- tageseinnahmen_live(pool)
+    }
+    geldformat(te)
+  })
+  output$tageseinnahmen_direkt2 <- renderUI({ geldformat(tageseinnahmen(pool, gestern_filter)) })
+  output$tageseinnahmen_direkt3 <- renderUI({ geldformat(tageseinnahmen(pool, vorgestern_filter)) })
+  output$tageseinnahmen_direkt4 <- renderUI({ geldformat(tageseinnahmen(pool, vorvorgestern_filter)) })
+  output$tageseinnahmen_vergleich1 <- renderUI({
+    dir <- tageseinnahmen(pool, heute_filter)
+    if (dir == 0) {
+      dir <- tageseinnahmen_live(pool)
+    }
+    vgl <- tageseinnahmen(pool, heute_vor_einer_woche_filter)
+    tageseinnahmen_vergleich(dir, vgl)
+  })
+  output$tageseinnahmen_vergleich2 <- renderUI({
+    tageseinnahmen_vergleich(tageseinnahmen(pool, gestern_filter),
+                             tageseinnahmen(pool, gestern_vor_einer_woche_filter))
+  })
+  output$tageseinnahmen_vergleich3 <- renderUI({
+    tageseinnahmen_vergleich(tageseinnahmen(pool, vorgestern_filter),
+                             tageseinnahmen(pool, vorgestern_vor_einer_woche_filter))
+  })
+  output$tageseinnahmen_vergleich4 <- renderUI({
+    tageseinnahmen_vergleich(tageseinnahmen(pool, vorvorgestern_filter),
+                             tageseinnahmen(pool, vorvorgestern_vor_einer_woche_filter))
   })
 }
--- ui.R
+++ ui.R
@@ -10,7 +10,8 @@
 library(shiny)
 library(shinymaterial)
 
-tag_namen <- c("Heute", "Gestern", "Vorgestern", "Vorvorgestern")
+tagesnamen <- c("Heute", "Gestern", "Vorgestern", "Vorvorgestern")
+heute <- Sys.Date()
 
 # Define UI for application that draws a histogram
 # Wrap shinymaterial apps in material_page
@@ -22,6 +23,7 @@ material_page(
   material_side_nav(
     fixed = TRUE,
     image_source = "Weltladen_Logo.png",
+    span(style = "display: none;", icon("caret-up")), # load the fontawesome icons dependencies
     material_row(
       material_column(
         width = 7,
@@ -84,27 +86,48 @@ material_page(
         material_column(
           width = 3,
           material_card(
-            title = HTML(
-              paste0(
-                "<span style='font-weight:bold; color:", "#9c27b0", "'>",
-                tag_namen[i], " (", format(Sys.Date() + 1 - i, "%A"), ")"
-                # "</span>&nbsp;&nbsp;",
-                # "<span style='font-size:14px'>", "Hier könnte ein Hinweis stehen", "</span>"
-              )
-            ),
+            title = HTML(paste0(
+              "<span style='font-weight:bold; color:", "#9c27b0", "'>",
+              tagesnamen[i], " (", format(heute + 1 - i, "%A"), ")</span>",
+              "<br>",
+              "<span style='font-size:14px'>", format(heute + 1 - i, "%x"), "</span>"
+            )),
             depth = 3,
-            HTML(
-              paste0(
-                "<div class='text-right'><span style='font-size:28px'>",
-                uiOutput(paste0("tageseinnahmen", i))
-                # "+",
-                # 42,
-                # "%"
-              ),
-              "</span></div>"
-            )
+            HTML(paste0(
+              "<div class='text-right'><span style='font-size:28px'>",
+              uiOutput(paste0("tageseinnahmen_direkt", i)),
+              "</span></div>",
+              "<div class='text-right'><span style='font-size:28px'>",
+              uiOutput(paste0("tageseinnahmen_vergleich", i)),
+              "</div>"
+            ))
           )
-        )                 
+        )
+      })
+    )
+  ),
+  material_row(
+    h4("Letzte Monatseinnahmen"),
+    tagList(
+      lapply(1:4, function(i) {
+        material_column(
+          width = 3,
+          material_card(
+            title = HTML(paste0(
+              "<span style='font-weight:bold; color:", "#9c27b0", "'>",
+              format(as.Date(paste0(substr(as.character(heute), 1, 8), "01")) - months(i - 1), "%B %Y"), "</span>"
+            )),
+            depth = 3,
+            HTML(paste0(
+              "<div class='text-right'><span style='font-size:28px'>",
+              uiOutput(paste0("monatseinnahmen_direkt", i)),
+              "</span></div>",
+              "<div class='text-right'><span style='font-size:28px'>",
+              uiOutput(paste0("monatseinnahmen_vergleich", i)),
+              "</div>"
+            ))
+          )
+        )
       })
     )
   ),
